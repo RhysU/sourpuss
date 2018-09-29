dbedc9b65fa0395f4694e679cb36417a219e4036
Rhys Ulerich <rhys.ulerich@gmail.com>
2018-09-28 21:35:59 -0400

    Add Click
    Following https://github.com/pallets/click/blob/master/examples/inout/inout.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 version = '0.1'
 
 install_requires = [
+    'click',
     'pandas',
 ]
 
--- src/sourpuss/__init__.py
+++ src/sourpuss/__init__.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+import click
 
 
-def main():
-    print("Hello, world!")
+@click.command()
+@click.argument('file', type=click.File('rb'), nargs=-1)
+def main(file):
+    """Cat a Python pickle file onto standard output, especially DataFrames."""
+    for f in file:
+        print(f)
+
+
+if __name__ == '__main__':
+    main()
