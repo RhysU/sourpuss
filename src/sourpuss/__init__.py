31a1d7d4725c6ef3b6b0f560bfbc9126a5ef3279
Rhys Ulerich <rhys.ulerich@gmail.com>
2018-09-28 22:39:26 -0400

    Convert to is_flag

--- src/sourpuss/__init__.py
+++ src/sourpuss/__init__.py
@@ -11,6 +11,8 @@ import pandas
 # TODO Add a path prefix as an index element
 # TODO Accept a directory and recursively dump pickles
 # TODO Convert all entries in the DataFrame into types via type(...)
+# TODO Accept a query string to reduce the data
+# TODO Permit sorting all rows
 
 
 # What types of paths can we hope to load successfully?
@@ -19,18 +21,27 @@ PicklePath = click.Path(exists=True, file_okay=True, dir_okay=False,
                         allow_dash=False, path_type=None)
 
 # What defaults are selected for configurable display parameters?
-DEFAULT_MULTI_SPARSE = False
 DEFAULT_PRECISION = 17
 
 
 @click.command()
 @click.argument('file', nargs=-1, type=PicklePath)
-@click.option('--multi_sparse', '-s', type=bool, show_default=True,
-              default=DEFAULT_MULTI_SPARSE)
+@click.option('--csv', '-c', is_flag=True,
+              help='Emit CSV instead of formatted table.')
+@click.option('--multi_sparse', '-s', is_flag=True,
+              help='Sparsify any MultiIndex display.')
 @click.option('--precision', '-p', type=int, show_default=True,
-              default=DEFAULT_PRECISION)
-def main(file, multi_sparse, precision):
+              metavar='DIGITS', default=DEFAULT_PRECISION,
+              help='Change precision for floating point.')
+def main(
+        file: typing.List[str],
+        *,
+        csv: typing.Optional[bool] = None,
+        multi_sparse: typing.Optional[bool] = None,
+        precision: typing.Optional[int] = None
+):
     """Cat Python pickle file(s) onto standard output, especially DataFrames."""
+
     with kid_gloves_off(multi_sparse=multi_sparse, precision=precision):
         for f in file:
             print(f)
@@ -39,9 +50,8 @@ def main(file, multi_sparse, precision):
 def kid_gloves_off(
         multi_sparse: typing.Optional[bool] = None,
         precision: typing.Optional[int] = None,
-):
+) -> pandas.option_context:
     """Like, seriously, Pandas just give me the entirety of my data."""
-
     return pandas.option_context(
         'display.date_yearfirst', True,
         'display.expand_frame_repr', True,
@@ -50,8 +60,7 @@ def kid_gloves_off(
         'display.max_colwidth', 1024,
         'display.max_rows', None,
         'display.max_seq_items', None,
-        'display.multi_sparse', (multi_sparse if multi_sparse is not None
-                                 else DEFAULT_MULTI_SPARSE),
+        'display.multi_sparse', bool(multi_sparse),
         'display.precision', (precision if precision is not None
                               else DEFAULT_PRECISION),
         'display.show_dimensions', False,
