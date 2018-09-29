223b81716e74d1fed8cab7099091501f3e8f35aa
Rhys Ulerich <rhys.ulerich@gmail.com>
2018-09-28 21:19:55 -0400

    Prepare setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,40 @@
+import os
+
 from setuptools import setup, find_packages
-import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
-
 version = '0.1'
 
 install_requires = [
-    # List your project dependencies here.
-    # For more details, see:
-    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    'pandas',
 ]
 
-
 setup(name='sourpuss',
-    version=version,
-    description="Like feeding pickles to a cat(1)",
-    long_description=README + '\n\n' + NEWS,
-    classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    ],
-    keywords='pandas cat pickle',
-    author='Rhys Ulerich',
-    author_email='rhys.ulerich@gmail.com',
-    url='https://github.com/RhysU/sourpuss',
-    license='Mozilla Public License 2.0',
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},include_package_data=True,
-    zip_safe=False,
-    install_requires=install_requires,
-    entry_points={
-        'console_scripts':
-            ['sourpuss=sourpuss:main']
-    }
-)
+      version=version,
+      description="Like feeding pickles to a cat(1)",
+      long_description=README + '\n\n' + NEWS,
+      classifiers=[
+          'Development Status :: 2 - Pre-Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Text Processing :: General',
+          'Topic :: Utilities',
+      ],
+      keywords='pandas cat pickle',
+      author='Rhys Ulerich',
+      author_email='rhys.ulerich@gmail.com',
+      url='https://github.com/RhysU/sourpuss',
+      license='Mozilla Public License 2.0',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      entry_points={'console_scripts': ['sourpuss=sourpuss:main']},
+      )
